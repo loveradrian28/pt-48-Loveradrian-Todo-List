e6ea8f3107673a88b57c6690ac9bee560d5eb237
Adrian Lovera <105324331+loveradrian28@users.noreply.github.com>
2023-11-09 10:48:00 +0000

    ToDo List

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+	const [userTodo, setUserTodo] = useState([]);
+  const [userInput, setUserInput] = useState('');
+
+  return (
+    <>
+      <div className="App__Container">
+        <h1>What to do today? </h1>
+        <div className="UserTodoList__Container">
+          <input
+            type="text"
+            onChange={(e) => setUserInput(e.target.value)}
+            value={userInput}
+            onKey={(e) => {
+              if (e.key === 'Enter') {
+                setUserTodo(userTodo.concat(userInput));
+                setUserInput('');
+              }
+            }}
+            placeholder="Hola vale que hay pa´hoy?"
+          ></input>
+          <button
+            onClick={(e) => {
+              setUserTodo(userTodo.concat(userInput));
+              setUserInput('');
+            }}
+          >
+            Add ToDo
+          </button>
+          {userTodo.map((TodoItem, index) => (
+            <li>
+              {TodoItem}{' '}
+              <FontAwesomeIcon
+                icon={faTrash}
+                onClick={() =>
+                  setUserTodo(
+                    userTodo.filter(
+                      (__, currentIndex) => index !== currentIndex
+                    )
+                  )
+                }
+              />
+            </li>
+          ))}
+          <div className="HowMany">{userTodo.length} TodosToDo</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
